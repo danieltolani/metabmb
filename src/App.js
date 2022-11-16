1bd287d42959c6899b609131b5a320f0b615ef34
Daniel Tolani <mumytosin2016@gmail.com>
2022-11-16 11:21:15 +0100

    update & Fix: Hero image scale on resize | nav bar responsivenss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import '../src/styles/App.css'
 import Layout from './components/Layout';
+import Home from './components/Home';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-     <Layout/>
-     <h1>HWLLLLL</h1>
-    </div>
+    <Router>
+      <div className='main-app'>
+      <Layout/>
+      <main className="app-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import '../styles/home.css'
+import Sponsors from './Sponsors';
+
+const Home = () => {
+    return(
+        <section className="home-section">
+            <div className="hero-container">
+                <div className="hero-left">
+                    <div className="hero-left-copy">
+                        <h1 className="hero-copy">Rent a <span className="text-secondary">Place</span> away from <span className="text-secondary">Home</span> in the <span className="text-secondary">Metaverse</span></h1>
+                        <p className="hero-par">we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
+                    </div>
+                    <form className="hero-search">
+                        <input placeholder="Search for location"/>
+                        <button className='search-button'>search</button>
+                    </form>
+                </div>
+
+                <div className='hero-right-container'>
+                    <div className="hero-right">
+                        <img className="hero-image grid-two" src="https://i.imgur.com/PNxllmF.png" alt="NFT_1" />
+                        <img className="hero-image" src="https://i.imgur.com/I2gaAWL.png" alt="NFT_2" />
+                        <img className="hero-image grid-two" src="https://i.imgur.com/EnOlvrM.png" alt="NFT_3" />
+                        <img className="hero-image" src="https://i.imgur.com/J73UDD6.png" alt="NFT_4" />
+                    </div>
+                </div>
+            </div>
+
+            <Sponsors />
+        </section>
+    )
+}
+
+export default Home;
